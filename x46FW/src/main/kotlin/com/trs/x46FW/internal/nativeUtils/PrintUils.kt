7bab4a4298137812699faa15e941172108f9ae26
Marie Antoinette <tetex505@gmail.com>
2023-09-09 19:05:18 -0400

    added and fix some stuff

--- x46FW/src/main/kotlin/com/trs/x46FW/internal/nativeUtils/PrintUils.kt
+++ x46FW/src/main/kotlin/com/trs/x46FW/internal/nativeUtils/PrintUils.kt
@@ -5,14 +5,6 @@ import java.lang.foreign.*
 object PrintUils
 {
 
-    /*init
-    {
-        val so_lib:String = "${System.getProperty("java.io.tmpdir")}/x46FWn-${Random.nextInt()}.so"
-        val f: File = File(so_lib)
-        f.writeBytes(getfile("JAR:/com/trs/x46FW/libx46FWn.so")?.readAllBytes()!!)
-       // f.deleteOnExit()
-        System.load(so_lib)
-    }*/
 
     fun printf(str: String):Long
     {
@@ -22,28 +14,22 @@ object PrintUils
             // 2. Dereference off-heap memory
             val nativeString = arena.allocateUtf8String(str)
 
-            // 3. Link and call C function
-
-            // 3a. Obtain an instance of the native linker
             val linker = Linker.nativeLinker()
 
-            // 3b. Locate the address of the C function
             val libc = linker.defaultLookup()
-            val strlen_addr: MemorySegment = libc.find("printf").get()
+            val printf_addr: MemorySegment = libc.find("printf").get()
 
-                // 3c. Create a description of the native function signature
-            val strlen_sig = FunctionDescriptor.of(
+
+            val printf_sig = FunctionDescriptor.of(
                 ValueLayout.JAVA_LONG,
                 ValueLayout.ADDRESS
-            )/*.ofVoid(
-                    ValueLayowut.ADDRESS
-                )*/
+            )
 
             // 3d. Create a downcall handle for the C function
-            val strlen = linker.downcallHandle(strlen_addr, strlen_sig)
+            val printf = linker.downcallHandle(printf_addr, printf_sig)
 
                 // 3e. Call the C function directly from Java
-            return@RET strlen.invokeExact(nativeString) as Long
+            return@RET printf.invokeExact(nativeString) as Long
         }
     }
 
---
+++ x46FW/src/test/kotlin/com/trs/x46FW/internal/nativeUtils/mem_test.kt
@@ -0,0 +1,13 @@
+package com.trs.x46FW.internal.nativeUtils
+
+import org.junit.jupiter.api.Test
+
+object mem_test
+{
+    @Test
+    fun mam()
+    {
+        /*val f = memTools.malloc(8)
+        memTools.free(f.address())*/
+    }
+}
